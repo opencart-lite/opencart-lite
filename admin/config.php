b1e1a7b8fc33d14a838189e331b310aaf241230a
ion <opencart.lite@gmail.com>
2014-06-12 22:51:10 +0300

    Initial commit
    Initialisation project

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart-lite/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart-lite/');
+define('HTTP_IMAGE', 'http://localhost/opencart-lite/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart-lite/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart-lite/');
+define('HTTPS_IMAGE', 'http://localhost/opencart-lite/image/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\wamp\www\opencart-lite/admin/');
+define('DIR_SYSTEM', 'D:\wamp\www\opencart-lite/system/');
+define('DIR_DATABASE', 'D:\wamp\www\opencart-lite/system/database/');
+define('DIR_LANGUAGE', 'D:\wamp\www\opencart-lite/admin/language/');
+define('DIR_TEMPLATE', 'D:\wamp\www\opencart-lite/admin/view/template/');
+define('DIR_CONFIG', 'D:\wamp\www\opencart-lite/system/config/');
+define('DIR_IMAGE', 'D:\wamp\www\opencart-lite/image/');
+define('DIR_CACHE', 'D:\wamp\www\opencart-lite/system/cache/');
+define('DIR_DOWNLOAD', 'D:\wamp\www\opencart-lite/download/');
+define('DIR_LOGS', 'D:\wamp\www\opencart-lite/system/logs/');
+define('DIR_CATALOG', 'D:\wamp\www\opencart-lite/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart-lite');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart-lite/');
+define('HTTP_IMAGE', 'http://localhost/opencart-lite/image/');
+define('HTTP_ADMIN', 'http://localhost/opencart-lite/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart-lite/');
+define('HTTPS_IMAGE', 'http://localhost/opencart-lite/image/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\wamp\www\opencart-lite/catalog/');
+define('DIR_SYSTEM', 'D:\wamp\www\opencart-lite/system/');
+define('DIR_DATABASE', 'D:\wamp\www\opencart-lite/system/database/');
+define('DIR_LANGUAGE', 'D:\wamp\www\opencart-lite/catalog/language/');
+define('DIR_TEMPLATE', 'D:\wamp\www\opencart-lite/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\wamp\www\opencart-lite/system/config/');
+define('DIR_IMAGE', 'D:\wamp\www\opencart-lite/image/');
+define('DIR_CACHE', 'D:\wamp\www\opencart-lite/system/cache/');
+define('DIR_DOWNLOAD', 'D:\wamp\www\opencart-lite/download/');
+define('DIR_LOGS', 'D:\wamp\www\opencart-lite/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart-lite');
+define('DB_PREFIX', '');
+?>
